b0e1e3fc912841c0ae540967b946fd3b9def548d
Trần Thanh Tùng <102464694+hoangtubongdem153@users.noreply.github.com>
2024-05-24 14:28:43 +0700

    Update TH1_Bai2.py

--- Algorithm_Python_code/TH1_Bai2.py
+++ Algorithm_Python_code/TH1_Bai2.py
@@ -1,3 +1,6 @@
+# Cho p=2147483647; W=8. Với t là độ dài từ của p, hãy nhập các giá trị a, b E [0, 2^Wt) từ bàn phím và AD thuật toán cộng chính xác bội 
+# để thực hiện tính giá trị c, với c = a + b mod 2^Wt.
+
 from Thu_Vien.phepToanVoiSoNguyenLon import *
 (w,p)=(8,2147483647)
 a = int(input('Nhập vào số a: '))
